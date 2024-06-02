84340084343d7ee34467781647f4424b152038ff
okluan <luanemerson8@gmail.com>
2024-06-02 17:13:08 -0300

    Java

---
+++ 2_exemplo/src/Exemplo2/Exemplo2.java
@@ -0,0 +1,9 @@
+package Exemplo2;
+
+public class Exemplo2 {
+
+		public static void main(String[] args) {
+	               System.out.println("Seu texto é inserido aqui, entre aspas duplas");
+	         }
+	
+}
